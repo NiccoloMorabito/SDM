c88a2f7b059b5dda4152bfdc5e976088daa8a922
NiccoloMorabito <morabito.niccolo@gmail.com>
2022-06-20 11:20:38 +0200

    Query for loading the data from Citrus to Neo4J

--- loader.py
+++ loader.py
@@ -41,12 +41,14 @@ driver.close()
 # follow this guide: https://neo4j.com/labs/apoc/4.3/database-integration/load-jdbc/ but mainly the following two steps:
     # download the postgresql jar driver
     # put it in the "Plugin" folder of the project
-# run the following query to move the data from citrus to neo4j:
+# run the following query to move the data from citrus to neo4j (TODO change the query on transactions to split the work)
 """
-WITH "jdbc:postgresql://clefable.fib.upc.edu:9700/bdm?user=postgres&password=postgres" as url
-CALL apoc.load.jdbc(url,"transactions") YIELD row
+WITH "jdbc:postgresql://clefable.fib.upc.edu:9700/bdm?user=postgres&password=postgres" as url,
+     //"select * from transactions where transaction_date between '1900-01-01' and '2100-12-31'" as query
+     "transactions" as query
+CALL apoc.load.jdbc(url,query) YIELD row
 MERGE (origin:Country {name:row.origin})
-MERGE (destination:Country {name:row.origin})
+MERGE (destination:Country {name:row.destination})
 CREATE (origin)-[r:transaction {id: row.id, description:row.description, price:row.price, quantity:row.quantity, unit:row.unit, date:row.transaction_date}]->(destination)
 """
 
